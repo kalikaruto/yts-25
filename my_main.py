d3aaf56f63acb231e1c9920e8c21f008d0eb0e5a
Haridas Mahato <haridasmahato12@gmail.com>
2026-06-06 15:07:29 +0530

    remove motor

--- my_main.py
+++ my_main.py
@@ -25,12 +25,6 @@ spi = SPI(
 cs = Pin(5, Pin.OUT)
 temperature = MAX6675(spi, cs)
 
-# setup motor
-step = Pin(2, Pin.OUT)
-dir = Pin(15, Pin.OUT)
-en = Pin(4, Pin.OUT)
-en.value(1) # disable for now
-
 # config values
 OPEN_THRESHOLD = 10
 CLOSE_THRESHOLD = 1700
@@ -42,10 +36,6 @@ WINDOW_UNKNOWN = -1
 LOG_FILE = "readings.csv"
 log_counter = 0
 
-# limit switches
-open_limit = Pin(23, Pin.IN, Pin.PULL_DOWN)
-close_limit = Pin(17, Pin.IN, Pin.PULL_DOWN)
-
 def get_local_time():
     t = time.localtime()
 
@@ -54,38 +44,6 @@ def get_local_time():
         t[3], t[4], t[5]
     )
     return timestamp_str
-
-def switch_pressed(pin):
-    if pin.value():
-        time.sleep_ms(20)
-        return pin.value()
-    return False
-
-def move_until_limit(limit_pin, delay=500, max_steps=1600):
-    en.value(0)
-
-    try:
-        steps = 0
-
-        while not switch_pressed(limit_pin):
-            step.value(1)
-            time.sleep_us(delay)
-
-            step.value(0)
-            time.sleep_us(delay)
-
-            steps += 1
-
-            if steps >= max_steps:
-                print("ERROR: limit switch not reached")
-                print("RESTART")
-                sys.exit(0)
-                return False
-
-        return True
-
-    finally:
-        en.value(1)
 
 def log_readings(temp, lumin):
     global log_counter
@@ -102,67 +60,17 @@ def log_readings(temp, lumin):
             temp,
             lumin
         ))
-    
-def open_window():
-    global window_state
-
-    if window_state == WINDOW_OPEN:
-        return
-
-    if switch_pressed(open_limit):
-        window_state = WINDOW_OPEN
-        return
-
-    print("opening")
-
-    dir.value(1)
-
-    success = move_until_limit(open_limit)
-
-    if success:
-        window_state = WINDOW_OPEN
-        print("fully opened")
-    else:
-        print("failed to open")
-
-
-def close_window():
-    global window_state
-
-    if window_state == WINDOW_CLOSED:
-        return
-
-    if switch_pressed(close_limit):
-        window_state = WINDOW_CLOSED
-        return
-
-    print("closing")
-
-    dir.value(0)
-
-    success = move_until_limit(close_limit)
-
-    if success:
-        window_state = WINDOW_CLOSED
-        print("fully closed")
-    else:
-        print("failed to close")
 
 def logic(temp, lumin):
     global window_state, log_counter
 
     print(f"Temperature: {temp:.2f} C\tLux: {lumin:.2f}")
 
-    if window_state == WINDOW_CLOSED:
-        if lumin < OPEN_THRESHOLD:
-            open_window()
+    if lumin < OPEN_THRESHOLD:
+        print("OPENING WINDOW")
 
-    elif window_state == WINDOW_OPEN:
-        if lumin > CLOSE_THRESHOLD:
-            close_window()
-    else:
-        print("UNKNOWN STATE")
-        sys.exit()
+    if lumin > CLOSE_THRESHOLD:
+        print("CLOSING WINDOW")
 
     log_counter+=1
     log_readings(temp, lumin)
@@ -174,26 +82,6 @@ try:
 except OSError:
     with open(LOG_FILE, "w") as f:
         f.write("timestamp,temp_c,lux\n")
-
-open_pressed = switch_pressed(open_limit)
-close_pressed = switch_pressed(close_limit)
-
-if open_pressed and close_pressed:
-    print("ERROR: both limit switches active")
-    window_state = WINDOW_UNKNOWN
-
-elif open_pressed:
-    window_state = WINDOW_OPEN
-
-elif close_pressed:
-    window_state = WINDOW_CLOSED
-
-else:
-    window_state = WINDOW_UNKNOWN
-
-if window_state == WINDOW_UNKNOWN:
-    print("Homing...")
-    close_window()
 
 while True:
     try:
